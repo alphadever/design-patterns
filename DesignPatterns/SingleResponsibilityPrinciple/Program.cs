afa6cfac50f99ff60a042b7b3e3777a8bc6fa117
alpha-dever <alphadever@gmail.com>
2018-03-12 13:42:13 -0400

    SRP logic
    SRP logic

--- DesignPatterns/SingleResponsibilityPrinciple/Program.cs
+++ DesignPatterns/SingleResponsibilityPrinciple/Program.cs
@@ -1,12 +1,79 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using static System.Console;
 
 namespace SingleResponsibilityPrinciple
 {
     class Program
     {
+        // just stores a couple of journal entries and ways of
+        // working with them
+        public class Journal
+        {
+            private readonly List<string> entries = new List<string>();
+
+            private static int count = 0;
+
+            public int AddEntry(string text)
+            {
+                entries.Add($"{++count}: {text}");
+                return count; // memento pattern!
+            }
+
+            public void RemoveEntry(int index)
+            {
+                entries.RemoveAt(index);
+            }
+
+            public override string ToString()
+            {
+                return string.Join(Environment.NewLine, entries);
+            }
+
+            // below methods break single responsibility principle below because 
+            // now the journal class is responsible for saving and loading which is not desirable
+            public void Save(string filename, bool overwrite = false)
+            {
+                File.WriteAllText(filename, ToString());
+            }
+
+            public void Load(string filename)
+            {
+
+            }
+
+            public void Load(Uri uri)
+            {
+
+            }
+        }
+
+        // handles the responsibility of persisting objects
+        public class Persistence
+        {
+            public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+            {
+                if (overwrite || !File.Exists(filename))
+                    File.WriteAllText(filename, journal.ToString());
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var j = new Journal();
+            j.AddEntry("Test Entry 1");
+            j.AddEntry("Test Entry 2");
+            WriteLine(j);
+
+            Process proc = new Process();
+            proc.StartInfo.UseShellExecute = true;
+            var p = new Persistence();
+            var filename = @"c:\temp\journal.txt";
+            proc.StartInfo.FileName = filename;
+            p.SaveToFile(j, filename);
+            proc.Start();
         }
     }
 }
